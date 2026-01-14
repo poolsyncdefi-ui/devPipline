e9dff268ad6fc7b4ed92899c87cb81d367c1f99b
poolsync.defi <poolsync.defi@gmail.com>
2026-01-14 16:19:10 +0100

    Hardhat compil KO

--- hardhat.config.mjs
+++ hardhat.config.mjs
@@ -1,10 +1,9 @@
-// hardhat.config.js - Version CommonJS
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config = {
   solidity: {
-    version: "0.8.19", // Compatible avec OpenZeppelin 4.9.5
+    version: "0.8.19",
     settings: {
       optimizer: {
         enabled: true,
@@ -15,27 +14,11 @@ const config = {
   networks: {
     hardhat: {
       chainId: 1337
-	  type: "edr-simulated"  // <-- AJOUTÉ (obligatoire dans v3)
-    },
-     localhost: {
-      url: "http://127.0.0.1:8545",
-      type: "http"  // <-- AJOUTÉ (obligatoire dans v3)
-    },
-    sepolia: {
-      url: process.env.ALCHEMY_API_KEY || "",
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
-      chainId: 11155111,
-      type: "http"  // <-- AJOUTÉ (obligatoire dans v3)
-    }
-  },
-  etherscan: {
-    apiKey: {
-      sepolia: process.env.ETHERSCAN_API_KEY || ""
     }
   },
   paths: {
     sources: "./contracts",
-    tests: "./test",
+    tests: "./tests",  # Important: avec 's'
     cache: "./cache",
     artifacts: "./artifacts"
   }
